9c119d07c7a41e4550d2c0f267e2cad59f65b21b
Carl Smith <carl.input@gmail.com>
2014-01-22 14:48:17 +0000

    Just trying to add config, but gitignore it afterwards...

--- hacks/config.py
+++ hacks/config.py
@@ -1,2 +1,15 @@
-# this file is executed immediately after the shell, so it can be used to
-# automate your setup, loading hacks and modules, setting names and so on
+'''
+nush config
+===========
+
+This file is executed immediately after the shell has finished loading. The code
+in this file is executed as the very last step in the nush boot process. You can
+use it to automate your nush setup, loading hacks and importing modules, setting
+handlers, and so on. It defines your default space.
+
+There's no special config system in nush. You just put code in this config file,
+which can load other code. This hook allows you to personalise the way that nush
+works when it starts up. You can reference all of nush's builtin stuff, like the
+shell and finder, in the code that you enter here.
+
+'''
